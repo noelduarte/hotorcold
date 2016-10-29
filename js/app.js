af6bb54a1725c05491745dca1b1acca8985e0262
noelduarte <noel.duarte@thinkful.com>
2016-10-29 16:51:29 -0400

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,41 @@
+/* Variables */
+var numberGuesses = 0;
+// var randomNumber = Math.floor((Math.random() * + 100) + 1);
+var randomNumber = 50;
 
-$(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+$("form").on("submit", function(event) {
+  event.preventDefault();
+  var $userGuess = +$(".user-guess").val();
 
-  	});
+  var difference = Math.abs($userGuess - randomNumber);
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+  var isNotANumber = isNaN($userGuess)
+
+  if (isNotANumber) {
+    $(".feedback").text("Type in a real number.")
+  } else {
+
+    runCheck()
+  }
+
+  function runCheck() {
+    numberGuesses += 1
+    $(".numberofguesses").text(numberGuesses)
+
+    if ($userGuess == randomNumber) {
+      $(".feedback").text("you got it! play again?")
+      $("form").trigger("reset");
+      randomNumber = Math.floor((Math.random() * 100) + 1);
+      numberGuesses = 0
+    } else if (difference <= 6) {
+      $(".feedback").text("hot")
+    } else if (difference <= 10) {
+      $(".feedback").text("warm")
+    } else if (difference <= 15) {
+      $(".feedback").text("lukewarm")
+    } else {
+      $(".feedback").text("cold")
+    }
+  }
 
 });
-
-
